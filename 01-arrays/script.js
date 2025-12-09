df058f7cb8715a5088da374855508798d37338ae
Zizou10 <paolo.pilati93@gmail.com>
2025-12-09 20:48:03 +0100

    1-7

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -13,20 +13,28 @@ const teachers = [
 const fourthTeacher = teachers.splice(3, 0, "Ermenegildo");
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+  teachers.splice(4, 1, "Patrick");
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+delete teachers[8];
+const lastTeacher = "Luca";
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+delete teachers[0];
+
+const firstTeacher = "Nathan";
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.push("Sarah");
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+
+const lewisIndex = teachers.indexOf("Lewis");
+  console.log(lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
